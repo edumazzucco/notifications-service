87ebab7c0534353ba81377ca3fd3c5ed3dd2d48f
edumazzucco <edumazzucco@hotmail.com>
2022-12-14 10:00:06 -0300

    test: notification test

---
+++ src/app/entities/notification.spec.ts
@@ -0,0 +1,23 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('Notification', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      recipientId: '123',
+      content: new Content('Hello world!'),
+      category: 'new_user',
+    });
+    expect(notification).toBeTruthy();
+  });
+
+  it('should be able to set a notification as read', () => {
+    const notification = new Notification({
+      recipientId: '123',
+      content: new Content('Hello world!'),
+      category: 'new_user',
+    });
+    notification.readAt = new Date();
+    expect(notification.readAt).toBeTruthy();
+  });
+});
