6f5ee1a532a26c09c5c553207c48542f80c192ff
edumazzucco <edumazzucco@hotmail.com>
2022-12-14 12:28:55 -0300

    feat: saving notification in memory

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,20 +1,18 @@
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    return Promise.resolve();
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
-    const response = await sendNotification.execute({
+
+    const { notification } = await sendNotification.execute({
       recipientId: '123',
       content: 'Hello world!',
       category: 'new_user',
     });
-    expect(response.notification).toBeTruthy();
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
