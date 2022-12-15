5e247bc520954ce7ad63342765f9a02e079e8112
edumazzucco <edumazzucco@hotmail.com>
2022-12-15 11:27:13 -0300

    feat: added get unread use case and tests

--- src/app/use-cases/unread-notification.spec.ts
+++ src/app/use-cases/unread-notification.spec.ts
@@ -1,33 +1,33 @@
 import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
-import { ReadNotification } from './read-notification';
+import { UnreadNotification } from './unread-notification';
 import { NotificationNotFound } from './errors/notification-not-found';
 
-describe('Read notification', () => {
-  it('should be able to read a notification', async () => {
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
-    const readNotification = new ReadNotification(notificationsRepository);
+    const unreadNotification = new UnreadNotification(notificationsRepository);
 
-    const notification = makeNotification();
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
 
     await notificationsRepository.create(notification);
 
-    await readNotification.execute({
+    await unreadNotification.execute({
       notificationId: notification.id,
     });
 
     expect(notificationsRepository.notifications).toHaveLength(1);
-    expect(notificationsRepository.notifications[0].readAt).toEqual(
-      expect.any(Date),
-    );
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
   });
 
-  it('should not be able to read a notification that does not exist', async () => {
+  it('should not be able to unread a notification that does not exist', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
-    const readNotification = new ReadNotification(notificationsRepository);
+    const unreadNotification = new UnreadNotification(notificationsRepository);
 
     await expect(
-      readNotification.execute({
+      unreadNotification.execute({
         notificationId: 'none',
       }),
     ).rejects.toThrow(NotificationNotFound);
